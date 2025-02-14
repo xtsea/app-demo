3bda742dc2897ba62f9cdda6357846eeff4f806e
Randy Dev <killerx@randydev.my.id>
2025-02-14 22:23:43 +0700

    _

--- app.py
+++ app.py
@@ -5,14 +5,3 @@ js_st = js.stl()
 js_st.title("Welcome to akenoai-lib APP")
 
 js.hide_streamlit_watermark(unsafe_allow_html=True)
-
-def run_async(func, *args):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    return loop.run_until_complete(func(*args))
-
-async def async_function():
-    return "Hello from Async"
-
-result = run_async(async_function)
-st.write(result)
